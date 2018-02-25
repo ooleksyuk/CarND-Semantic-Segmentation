d81aabbf4125d935de70299be7129f4a167c8458
Olga Oleksyuk <olga@oleksyuk.me>
2018-02-25 16:44:39 -0500

    Remove random crop

--- helper_cityscapes.py
+++ helper_cityscapes.py
@@ -126,7 +126,7 @@ def gen_batch_function(data_folder, image_shape):
             for image_file in image_paths[batch_i:batch_i + batch_size]:
                 gt_image = cv2.imread(os.path.join(gt_dataset_dir, image_file))
                 image = cv2.imread(os.path.join(train_dataset_dir, image_file[:-5] + '.png'))
-                image, gt_image = random_crop(image, gt_image)  # Random crop augmentation
+                # image, gt_image = random_crop(image, gt_image)  # Random crop augmentation
                 image = cv2.resize(image, image_shape)
                 gt_image = cv2.resize(gt_image, image_shape)
                 contr = random.uniform(0.85, 1.15)  # Contrast augmentation
