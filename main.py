05d45d462eb6ad25d1e0c8f443acb06974b3e9a5
Olga Oleksyuk <olga@oleksyuk.me>
2018-02-26 08:11:35 -0500

    Reshape weights from 4x1 to 2x2

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     # train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
     #
     # return logits, train_op, cross_entropy_loss
-    weights = tf.reshape([0.3, 0.3, 0.3, 0.6], (2, 3), name='weights')
+    weights = tf.reshape([0.3, 0.3, 0.3, 0.6], (2, 2), name='weights')
     logits = tf.reshape(nn_last_layer, (-1, num_classes), name='logits')
     labels = tf.reshape(correct_label, (-1, num_classes), name='labels')
 
